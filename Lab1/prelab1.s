90e9683cca1d3dd2396a92d6310e62e2647aa57c
Jane <janekennerly@gmail.com>
2022-10-09 17:12:55 -0700

    Added Lab1 prelab files

---
+++ Lab1/prelab1.s
@@ -0,0 +1,10 @@
+	AREA prelab1, CODE, READONLY     ; Run in code section. So only READONLY, name of the assemble code prelab1 
+	THUMB  ; THUMB instructions permitted
+	ENTRY
+	EXPORT __main ; Enter to main() function
+
+__main 	PROC ; Definition of main() function
+		MOV r0, #8
+		MOV r1, #5
+		ADD r2, r0, r1
+		END
